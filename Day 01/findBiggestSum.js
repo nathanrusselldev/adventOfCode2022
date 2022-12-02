55f4cedfe5819ea8a4ee1d0d71052722211849d7
nathanrusselldev <nathanrussell.it@gmail.com>
2022-12-02 18:06:17 -0500

    git push origin main

---
+++ Day 01/findBiggestSum.js
@@ -0,0 +1,13 @@
+const sortMe = require('./output')
+
+const findBiggestSum = (array) => {
+    // console.log(array)
+    const reducedArray = []
+    for(const item of array) {
+        let currentEle = item.reduce((a, b) => a + b)
+        reducedArray.push(currentEle)
+    }
+    console.log(reducedArray.sort((a, b) => a-b).pop())
+    return reducedArray.sort((a, b) => a-b).pop()
+} 
+findBiggestSum(sortMe)
