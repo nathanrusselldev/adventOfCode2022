3044076fc4ce59f70f2facc719d603e89373327e
nathanrusselldev <nathanrussell.it@gmail.com>
2022-12-02 23:35:03 -0500

    added part two solution

--- Day 01/findBiggestSum.js
+++ Day 01/findBiggestSum.js
@@ -13,4 +13,18 @@ const findBiggestSum = (array) => {
     console.log(reducedArray.sort((a, b) => a-b).pop())
     return reducedArray.sort((a, b) => a-b).pop()
 } 
+
 findBiggestSum(sortMe)
+
+const findBiggestThree = (array) => {
+    // console.log(array)
+    const reducedArray = []
+    for(const item of array) {
+        let currentEle = item.reduce((a, b) => a + b)
+        reducedArray.push(currentEle)
+    }
+    const sorted = reducedArray.sort((a, b) => a-b)
+    console.log(sorted[sorted.length-1] + sorted[sorted.length-2] + sorted[sorted.length-3])
+} 
+
+findBiggestThree(sortMe)
